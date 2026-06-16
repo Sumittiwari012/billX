867c2a88f8a262ad0293885216f7460e4255b0c9
Sumittiwari012 <st386700@gmail.com>
2026-06-16 17:40:16 +0530

    wholesalepricecolumn migration added

--- obj/Debug/net8.0-windows/Views/ProductLabelPrintWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/ProductLabelPrintWindow.g.i.cs
@@ -132,7 +132,7 @@ namespace MyWPFCRUDApp.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyWPFCRUDApp;V1.2.37.0;component/views/productlabelprintwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyWPFCRUDApp;V1.2.38.0;component/views/productlabelprintwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ProductLabelPrintWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
