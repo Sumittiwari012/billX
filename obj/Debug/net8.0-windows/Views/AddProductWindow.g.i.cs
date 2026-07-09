1d2e04a90d2a6ee074f0189d82f13d29abd2fbb6
Sumittiwari012 <st386700@gmail.com>
2026-07-09 15:32:42 +0530

    barcode updated

--- obj/Debug/net8.0-windows/Views/AddProductWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/AddProductWindow.g.i.cs
@@ -84,7 +84,7 @@ namespace MyWPFCRUDApp.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyWPFCRUDApp;V1.2.59.0;component/views/addproductwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyWPFCRUDApp;V1.2.60.0;component/views/addproductwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\AddProductWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
